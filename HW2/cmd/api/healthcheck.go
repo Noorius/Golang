3433427d9f15443e5bd3b390ca79681c5b4b8fa3
Noorius <n_zhetesov@kbtu.kz>
2023-10-05 11:15:45 +0600

    Add files via upload

--- HW2/cmd/api/healthcheck.go
+++ HW2/cmd/api/healthcheck.go
@@ -1,16 +1,29 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
 func (app *Application) HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
-	js := `{"status" : "available", "environment" : %q, "version": %q}`
-	js = fmt.Sprintf(js, app.config.env, version)
+	data := map[string]string{
+		"status":      "available",
+		"environment": app.config.env,
+		"version":     version,
+	}
+	js, err := json.Marshal(data)
+
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "Server cannot process your request", http.StatusInternalServerError)
+		return
+	}
+
+	js = append(js, '\n')
 
 	w.Header().Set("Content-Type", "application/json")
-	w.Write([]byte(js))
+	w.Write(js)
 }
 
 func (app *Application) CountHandler(w http.ResponseWriter, r *http.Request) {
