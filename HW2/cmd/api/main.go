b4faa63421e785d639a6607fc2478b3540014928
Noorius <n_zhetesov@kbtu.kz>
2023-10-12 17:40:09 +0600

    Add files via upload

--- HW2/cmd/api/main.go
+++ HW2/cmd/api/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
+	_ "github.com/lib/pq"
 	"log"
 	"net/http"
 	"os"
@@ -16,6 +19,9 @@ var cnt = 0
 type config struct {
 	port int
 	env  string
+	db   struct {
+		dsn string
+	}
 }
 
 type Application struct {
@@ -27,8 +33,19 @@ func main() {
 	var cfg config
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+	flag.StringVar(&cfg.db.dsn, "db-dsn", "postgres://greenlight:pa55word@localhost/greenlight?sslmode=disable", "PostgreSQL DSN")
 	flag.Parse()
+
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	defer db.Close()
+
+	logger.Printf("database connection pool established")
 
 	app := &Application{
 		config: cfg,
@@ -43,7 +60,25 @@ func main() {
 		WriteTimeout: 30 * time.Second,
 	}
 
-	logger.Println(fmt.Sprintf("starting server on port %d", srv.Addr))
-	err := srv.ListenAndServe()
+	logger.Println(fmt.Sprintf("starting %s server on port %s", cfg.env, srv.Addr))
+	err = srv.ListenAndServe()
 	logger.Fatal(err)
+}
+
+func openDB(cfg config) (*sql.DB, error) {
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+
+	defer cancel()
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
