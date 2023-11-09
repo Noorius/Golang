2c313b75031a19318fd019ad380ff1bd3da7d148
Noorius <n_zhetesov@kbtu.kz>
2023-11-09 16:05:55 +0600

    Add files via upload

---
+++ HW2/cmd/api/context.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"context"
+	"hw2.nur.net/internal/data"
+	"net/http"
+)
+
+type contextKey string
+
+const userContextKey = contextKey("user")
+
+func (app *Application) contextSetUser(r *http.Request, user *data.User) *http.Request {
+	ctx := context.WithValue(r.Context(), userContextKey, user)
+	return r.WithContext(ctx)
+}
+
+func (app *Application) contextGetUser(r *http.Request) *data.User {
+	user, ok := r.Context().Value(userContextKey).(*data.User)
+	if !ok {
+		panic("missing user value in request context")
+	}
+	return user
+}
