3c8c61b9c2b60b9b38d44eee40fe3fe6bcb613fd
Noorius <n_zhetesov@kbtu.kz>
2023-11-09 15:40:06 +0600

    Add files via upload

--- HW2/cmd/api/routes.go
+++ HW2/cmd/api/routes.go
@@ -20,5 +20,7 @@ func (app *Application) routes() http.Handler {
 
 	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
 
+	router.HandlerFunc(http.MethodPut, "/v1/users/activated", app.activateUserHandler)
+
 	return app.recoverPanic(app.rateLimit(router))
 }
--- HW2/internal/data/models.go
+++ HW2/internal/data/models.go
@@ -12,12 +12,14 @@ var (
 
 type Models struct {
 	Knives KnifeModel
+	Tokens TokenModel
 	Users  UserModel
 }
 
 func NewModels(db *sql.DB) Models {
 	return Models{
 		Knives: KnifeModel{DB: db},
+		Tokens: TokenModel{DB: db},
 		Users:  UserModel{DB: db},
 	}
 }
