f71b94d3ef1071bfd46b03b8609d9252b9dda6fa
Noorius <n_zhetesov@kbtu.kz>
2023-11-10 10:38:06 +0600

    Add files via upload

--- HW2/cmd/api/main.go
+++ HW2/cmd/api/main.go
@@ -12,6 +12,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 	"sync"
 	"time"
 )
@@ -40,6 +41,9 @@ type config struct {
 		username string
 		password string
 		sender   string
+	}
+	cors struct {
+		trustedOrigins []string
 	}
 }
 
@@ -70,6 +74,11 @@ func main() {
 	flag.StringVar(&cfg.smtp.username, "smtp-username", "e8d3297a6409a0", "SMTP username")
 	flag.StringVar(&cfg.smtp.password, "smtp-password", "7194a36fc12286", "SMTP password")
 	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "Greenlight <no-reply@greenlight.alexedwards.net>", "SMTP sender")
+
+	flag.Func("cors-trusted-origins", "Trusted CORS origins (space separated)", func(val string) error {
+		cfg.cors.trustedOrigins = strings.Fields(val)
+		return nil
+	})
 
 	flag.Parse() // os.Getenv("GREENLIGHT_DB_DSN")
 
